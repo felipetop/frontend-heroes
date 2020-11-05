3b5e75c3e932743226d6c7b57657a7e76864d9af
felipetop <top_cs@hotmail.com>
2020-11-04 23:15:33 -0300

    Add reset css and start customise

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="container">
+      <h1>Hello World</h1>
+      <h2>Hello World</h2>
+      <h3>Hello World</h3>
+      <h4>Hello World</h4>
+      <h5>Hello World</h5>
+      <h6>Hello World</h6>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu ante ut enim facilisis laoreet. 
+        Vivamus ac cursus augue, sit amet convallis metus. Phasellus eget ligula id sem fermentum lacinia nec 
+        in lectus. Pellentesque ac posuere risus. Vestibulum feugiat aliquet nisl, ac gravida sapien placerat at. Cras urna ante, lacinia eu venenatis et, bibendum ultricies leo. Vivamus tincidunt mollis leo, ut ullamcorper odio dapibus ut. Donec consectetur ante turpis, sit amet lacinia dolor bibendum nec. Pellentesque facilisis euismod sagittis. Nulla eget mauris ex. Etiam eu dictum sem.
+      </p>
+      <p>
+        Praesent ac luctus nibh. Duis turpis augue, ullamcorper non malesuada ac, feugiat eleifend metus. 
+        Sed iaculis tempus nisl faucibus faucibus. Praesent lorem nulla, gravida vel accumsan lacinia, 
+        malesuada eget justo. Aenean at cursus nulla, nec ullamcorper metus. Vivamus massa risus, 
+        vestibulum quis dignissim tincidunt, condimentum ut risus. Donec ut tincidunt dui, nec faucibus eros. 
+        Etiam fringilla in dui suscipit placerat. Nunc nec elit mattis ex dignissim dictum. Nulla tempus, 
+        sem non tempor euismod, tellus dui sagittis enim, eget blandit eros sem nec nisl.
+      </p>
+      <p>
+        Cras dignissim eu tortor non euismod. Quisque et enim dui. Nullam suscipit est vel libero tristique 
+        sagittis. Curabitur congue purus quis leo ullamcorper bibendum. Duis quis hendrerit magna. Fusce vitae 
+        arcu dapibus, eleifend erat in, aliquet tortor. Aliquam accumsan aliquam magna, ut vestibulum elit 
+        pretium sed. Donec in lorem at ligula consectetur tempor. Suspendisse elementum, eros id feugiat sodales, 
+        arcu arcu dapibus ante, nec tincidunt nisi ex in orci. In hac habitasse platea dictumst. 
+        Vestibulum non magna nec sem pulvinar egestas. Proin malesuada accumsan arcu a lobortis. Sed pulvinar 
+        gravida quam ac vulputate.
+      </p>
+    </section>
   );
 }
 
