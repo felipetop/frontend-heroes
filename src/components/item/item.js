52508aa5c76fc30155b32cb2e345091d554aa36f
felipetop <top_cs@hotmail.com>
2020-11-10 00:39:00 -0300

    Add first 20 results of Marvel Hero API

--- src/components/item/item.js
+++ src/components/item/item.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
-import image from './example.png';
 
 class Panel extends Component {
   render() {
     return (
       <div className="Item">
         <div>
-          <img src={image}/>
+          <img src={this.props.image}/>
           <div className="itemText"> 
-            <h3>Star - Lord</h3>
+            <h3>{this.props.name}</h3>
             <div>
               <input type="checkbox"/>
             </div>
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Footer from '../layout/footer/Footer';
 import Header from '../layout/header/Header';
 import GridContainer from '../components/gridContainer/gridContainer';
 import Item from '../components/item/item';
 import Search from '../components/search/search.js';
 import Panel from '../components/panel/panel.js';
+import axios from 'axios';
 
 function Index() {
+  const [heroList, setHeroList] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://gateway.marvel.com/v1/public/characters?ts=1&limit=20&apikey=edc13921ce2d3563dcd2a150281f5c23&hash=1acefe488f2631cef41c7443649d0483')
+    .then(function (response) {
+      // handle success
+      console.log(response.data.data.results);
+      setHeroList(response.data.data.results)
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+  }, []);
+
+  const renderHeroes = () => {
+    return heroList.map((hero) => {
+      return (
+        <div>
+          <Item name={ hero.name } image={hero.thumbnail.path+'.'+hero.thumbnail.extension} />
+        </div>
+      )
+    });
+  }
+
   return (
     <div className="FrontendHeroes">
       <Header className="container" title="Explore o universo">
@@ -19,13 +45,7 @@ function Index() {
       <Panel className="container mt-50" />
       <section className="container">
         <GridContainer grids={4} mobileGrids={2} verticalMargin={5} horizontalMargin={25} bottomMargin={40}>
-          <Item/>
-          <Item/>
-          <Item/>
-          <Item/>
-          <Item/>
-          <Item/>
-          <Item/>
+          {renderHeroes()}
         </GridContainer>
       </section>
       <Footer/>
