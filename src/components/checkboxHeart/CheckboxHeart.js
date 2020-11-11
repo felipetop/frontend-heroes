fdce5ee356b70ea2db5cf35a94a2fd284c44938e
felipetop <top_cs@hotmail.com>
2020-11-10 21:50:11 -0300

    fix heart icon

--- src/components/checkboxHeart/CheckboxHeart.js
+++ src/components/checkboxHeart/CheckboxHeart.js
@@ -4,15 +4,15 @@ function CheckboxHeart(props) {
     const [checked, setChecked] = useState(false);
     return (
       <div className={props.className}>
-        <input 
-          type="checkbox"
-          class='red-heart-checkbox' type='checkbox'
-          id={props.id}
-          checked={!checked}
-          onChange={() => setChecked(!checked)}
-        />
-        <label htmlFor={props.id}>
-        </label>
+          <input 
+            type="checkbox"
+            class='red-heart-checkbox' type='checkbox'
+            id={props.id}
+            checked={!checked}
+            onChange={() => setChecked(!checked)}
+          />
+          <label htmlFor={props.id}>
+          </label>
       </div>
     );
 }
